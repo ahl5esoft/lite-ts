96b2f4526c3e442698d3d414b37408b95830b8ca
ahl5esoft <ahl5esoft@gmail.com>
2021-02-13 16:55:06 +0800

    docs: v0.1.7

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ![Version](https://img.shields.io/badge/version-0.1.6-green.svg)
+# ![Version](https://img.shields.io/badge/version-0.1.7-green.svg)
--- src/dp/cor-base.ts
+++ src/dp/cor-base.ts
@@ -3,15 +3,17 @@ type NextType = CORBase | (() => CORBase);
 export abstract class CORBase {
     private m_Nexts: NextType[] = [];
 
-    protected break = false;
+    public break = false;
 
     public async handle(): Promise<void> {
+        if (this.break)
+            return;
+
         for (const r of this.m_Nexts) {
             let handler = r instanceof CORBase ? r : r();
+            await handler.handle();
             if (handler.break)
                 break;
-
-            await handler.handle();
         }
     }
 
