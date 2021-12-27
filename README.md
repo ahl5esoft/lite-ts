c022e93fc1360dbb95e74c893b183d77cb6331b2
ahl5esoft <ahl5esoft@gmail.com>
2021-12-27 14:50:14 +0800

    feat: add authData

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# ![Version](https://img.shields.io/badge/version-8.39.19-green.svg)
+# ![Version](https://img.shields.io/badge/version-8.40.19-green.svg)
 
 服务端通用库
--- src/model/enum/header.ts
+++ src/model/enum/header.ts
@@ -1,4 +1,5 @@
 export enum Header {
+    authData = 'H-A-D', // auth is 认证数据
     authToken = 'H-A-T', // authToken is 认证令牌
     env = 'H-E', // env is 环境
     timeout = 'H-T', // timeout is 超时
