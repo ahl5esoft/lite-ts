199609a3dff83f934fbfe189b63e4a37bf156c55
linyimin <ahl5esoft@gmail.com>
2022-09-30 18:03:38 +0800

    fix: 负载BUG

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ![Version](https://img.shields.io/badge/version-14.196.64-green.svg)
+# ![Version](https://img.shields.io/badge/version-14.196.65-green.svg)
 
 服务端通用库
 
--- src/service/bent/config-loader.ts
+++ src/service/bent/config-loader.ts
@@ -2,20 +2,24 @@ import bent from 'bent';
 import moment from 'moment';
 
 import { ConfigLoaderBase } from '../../contract';
+import { config } from '../../model';
 
 /**
  * 配置加载器
  */
 export class BentConfigLoader extends ConfigLoaderBase {
     /**
-     * 获取函数
+     * 负载
      */
-    private m_GetFunc: bent.RequestFunction<bent.Json>;
-
+    private m_LoadBalance: any;
     /**
      * 下次拉取时间
      */
     private m_NextPullOn: number;
+    /**
+     * 获取函数
+     */
+    private m_GetFunc: bent.RequestFunction<bent.Json>;
 
     /**
      * 构造函数
@@ -31,13 +35,15 @@ export class BentConfigLoader extends ConfigLoaderBase {
         this.m_NextPullOn = 0;
     }
 
-    private m_LoadBalance: any;
     /**
      * 加载
      * 
      * @param ctor 模型
      */
     public async load<T>(ctor: new () => T) {
+        if (ctor.name != config.LoadBalance.name)
+            return;
+
         const now = moment().unix();
         if (this.m_NextPullOn < now) {
             try {
@@ -45,6 +51,7 @@ export class BentConfigLoader extends ConfigLoaderBase {
             } catch { }
             this.m_NextPullOn = now + 60;
         }
+
         return this.m_LoadBalance;
     }
 }
