89e784cd76fb56fd75d97554fb533047fc4abd9d
linyimin <ahl5esoft@gmail.com>
2022-10-18 13:50:16 +0800

    feat: #1870

--- src/contract/i-user-reward-service.ts
+++ src/contract/i-user-reward-service.ts
@@ -1,25 +1,11 @@
 import { IUnitOfWork } from './i-unit-of-work';
 import { contract } from '../model';
 
-/**
- * 用户奖励服务
- */
 export interface IUserRewardService {
-    /**
-     * 获取结果
-     * 
-     * @param uow 工作单元
-     * @param rewards 奖励
-     * @param source 来源
-     * @param scene 场景
-     */
     findResults(uow: IUnitOfWork, rewards: contract.IReward[][], source: string, scene?: string): Promise<contract.IValue[]>;
-    /**
-     * 预览
-     * 
-     * @param uow 工作单元
-     * @param rewardsGroup 奖励组
-     * @param scene 场景
-     */
+    findResultsWithIndex(uow: IUnitOfWork, rewards: contract.IReward[][], source: string, scene?: string): Promise<[contract.IValue[], number]>;
     preview(uow: IUnitOfWork, rewardsGroup: { [key: string]: contract.IReward[][] }, scene?: string): Promise<{ [key: string]: contract.IValue[] }>;
+    previewWithIndex(uow: IUnitOfWork, rewardsGroup: { [key: string]: contract.IReward[][] }, scene?: string): Promise<{
+        [key: string]: [contract.IValue[], number]
+    }>;
 }
--- src/service/db/user-reward-service_test.ts
+++ src/service/db/user-reward-service_test.ts
@@ -93,6 +93,99 @@ describe('src/service/db/user-reward-service.ts', () => {
         });
     });
 
+    describe('.findResultsWithIndex(uow: IUnitOfWork, rewards: IRewardData[][], scene?: string)', () => {
+        it('固定', async () => {
+            const mockUserService = new Mock<UserServiceBase>();
+            const self = new Self(mockUserService.actual, null);
+
+            const mockRandSeedService = new Mock<IUserRandSeedService>();
+            mockUserService.expectReturn(
+                r => r.getRandSeedService(''),
+                mockRandSeedService.actual
+            );
+
+            Reflect.set(self, 'findOpenRewards', (_: IUnitOfWork, arg: number) => {
+                return arg == 1 ? [
+                    [{
+                        count: 3,
+                        valueType: 4,
+                    }]
+                ] : null;
+            });
+
+            mockUserService.expectReturn(
+                r => r.getRandSeedService(''),
+                mockRandSeedService.actual
+            );
+
+            mockUserService.expectReturn(
+                r => r.getRandSeedService(''),
+                mockRandSeedService.actual
+            );
+
+            const res = await self.findResultsWithIndex(null, [
+                [{
+                    count: 2,
+                    valueType: 1
+                }]
+            ], 'test');
+            deepStrictEqual(res, [
+                [{
+                    count: 6,
+                    source: 'test',
+                    targetNo: undefined,
+                    targetType: undefined,
+                    valueType: 4,
+                }],
+                0
+            ]);
+        });
+
+        it('权重', async () => {
+            const mockUserService = new Mock<UserServiceBase>();
+            const self = new Self(mockUserService.actual, null);
+
+            const mockRandSeedService = new Mock<IUserRandSeedService>();
+            mockUserService.expectReturn(
+                r => r.getRandSeedService(''),
+                mockRandSeedService.actual
+            );
+
+            Reflect.set(self, 'findOpenRewards', () => {
+                return null;
+            });
+
+            mockRandSeedService.expectReturn(
+                r => r.use(null, 2),
+                11
+            );
+
+            const res = await self.findResultsWithIndex(null, [
+                [{
+                    count: 2,
+                    source: 't2',
+                    valueType: 1,
+                    weight: 10
+                }, {
+                    count: 2,
+                    source: 't2',
+                    valueType: 3,
+                    weight: 11
+                }]
+            ], 'test');
+            deepStrictEqual(res, [
+                [{
+                    count: 2,
+                    source: 't2',
+                    targetNo: undefined,
+                    targetType: undefined,
+                    valueType: 3,
+                }],
+                1,
+            ]);
+        });
+    });
+
     describe(`.preview(uow: IUnitOfWork, rewardsGroup: { [key: string]: contract.IReward[][] }, scene = '')`, () => {
         it('ok', async () => {
             const mockUserService = new Mock<UserServiceBase>();
@@ -157,6 +250,79 @@ describe('src/service/db/user-reward-service.ts', () => {
                     count: 6,
                     valueType: 5
                 }]
+            });
+            strictEqual(rewards.length, 1);
+        });
+    });
+
+
+    describe(`.previewWithIndex(uow: IUnitOfWork, rewardsGroup: { [key: string]: contract.IReward[][] }, scene = '')`, () => {
+        it('ok', async () => {
+            const mockUserService = new Mock<UserServiceBase>();
+            const self = new Self(mockUserService.actual, null);
+
+            const mockRandSeedService = new Mock<IUserRandSeedService>();
+            mockUserService.expectReturn(
+                r => r.getRandSeedService('test'),
+                mockRandSeedService.actual
+            );
+
+            Reflect.set(self, 'findOpenRewards', (_: IUnitOfWork, arg: number) => {
+                return arg == 1 ? [
+                    [{
+                        count: 4,
+                        valueType: 3,
+                        weight: 1
+                    }, {
+                        count: 6,
+                        valueType: 5,
+                        weight: 1
+                    }]
+                ] : null;
+            });
+
+            mockRandSeedService.expectReturn(
+                r => r.get(null, 2, 0),
+                11
+            );
+
+            mockRandSeedService.expectReturn(
+                r => r.get(null, 1, 2),
+                0
+            );
+
+            mockRandSeedService.expectReturn(
+                r => r.get(null, 1, 3),
+                1
+            );
+
+            const rewards = [
+                [{
+                    count: 10,
+                    source: 't2',
+                    valueType: 1,
+                    weight: 1
+                }, {
+                    count: 2,
+                    source: 't2',
+                    valueType: 1,
+                    weight: 9
+                }]
+            ];
+            const res = await self.previewWithIndex(null, {
+                '': rewards
+            }, 'test');
+            deepStrictEqual(res, {
+                '': [
+                    [{
+                        count: 4,
+                        valueType: 3
+                    }, {
+                        count: 6,
+                        valueType: 5
+                    }],
+                    0
+                ]
             });
             strictEqual(rewards.length, 1);
         });
